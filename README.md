6e42d504e24c2585c2b01fe32c5d0555bd6e7657
logycoconut <1425795337@qq.com>
2020-08-18 11:09:26 +0800

    update README

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
-### Hi there 👋
 
-<!--
-**logycoconut/logycoconut** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+```
+    __                                              __ 
+   / /___  ____ ___  ___________  _________  __  __/ /_
+  / / __ \/ __ `/ / / / ___/ __ \/ ___/ __ \/ / / / __/
+ / / /_/ / /_/ / /_/ / /__/ /_/ / /__/ / / / /_/ / /_  
+/_/\____/\__, /\__, /\___/\____/\___/_/ /_/\__,_/\__/  
+        /____//____/                                                                    
+```
+## Latest blog posts
+[SpringBoot实现自己的拦截器](/2020/015_spring_handlerinterceptoradapter/) - August 12, 2020
 
-Here are some ideas to get you started:
+[SpringMVC的参数验证中BindingResult的使用](/2020/014_spring_bindingresult/) - July 21, 2020
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+[记一次黑苹果安装过程](/2020/012_hackintoshinstall/) - June 14, 2020
+
+[如何实现无状态登录](/2020/011_token_rsa/) - February 21, 2020
+
+[Zuul的鉴权以及微服务的跨域问题](/2020/010_spring_boot_gateway/) - February 2, 2020
+
+[一个应用集](/2020/009_necessary_software/) - January 14, 2020
+
+[Spring Security折腾小记🐵](/2020/008_spring_security_notes/) - January 2, 2020
+
+
+[>>> More](https://logycoco.xyz/posts/)
+## Statistics
+![Stats](https://github-readme-stats.vercel.app/api?username=logycoconut)
+![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=logycoconut&hide=html&layout=compact)
--- generate.py
+++ generate.py
@@ -1,34 +1,35 @@
 import requests
 from lxml import etree
 
-text = requests.get('https://logycoco.xyz/posts/').text
-html = etree.HTML(text)
+resp = requests.get('https://logycoco.xyz/posts/')
+resp.encoding = 'utf-8'
+html = etree.HTML(resp.text)
 
-with open('README.md', 'w') as f:
+with open('README.md', 'w', encoding='utf-8') as f:
     f.write(r'''
-            ```
+```
     __                                              __ 
    / /___  ____ ___  ___________  _________  __  __/ /_
   / / __ \/ __ `/ / / / ___/ __ \/ ___/ __ \/ / / / __/
  / / /_/ / /_/ / /_/ / /__/ /_/ / /__/ / / / /_/ / /_  
 /_/\____/\__, /\__, /\___/\____/\___/_/ /_/\__,_/\__/  
         /____//____/                                                                    
-            ```
-            ## Latest blog posts
-            ''')
+```
+## Latest blog posts
+''')
 
-    items = html.xpath("//article[@class='archive-item']")
+    items = html.xpath("//article[@class='archive-item']")[:7]
 
-    for item in items :
+    for item in items:
         title = item.xpath("a/text()")[0]
         href = item.xpath("a/@href")[0]
-        date = item.xpath("span/text()")[0]
-        
-        f.write('[{}]({}) - {}\n'.format(title, href, date))
+        date = item.xpath("span/text()")[0].strip()
+
+        f.write('[{}]({}) - {}\r\n'.format(title, href, date))
 
     f.write('''
-    [>>> More blog posts](https://logycoco.xyz/posts/)
-    ## Statistics
-    ![Stats](https://github-readme-stats.vercel.app/api?username=logycoconut)
-    ![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=logycoconut&hide=html&layout=compact)
-    ''')
+[>>> More](https://logycoco.xyz/posts/)
+## Statistics
+![Stats](https://github-readme-stats.vercel.app/api?username=logycoconut)
+![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=logycoconut&hide=html&layout=compact)
+''')
